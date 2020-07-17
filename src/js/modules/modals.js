a6665a9f59720580e144a05e1b7464188244aabf
WIN-0FKKBPC828E\User <neox56@gmail.com>
2020-07-17 12:58:22 +0400

    feat: add tabs.js

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -35,5 +35,5 @@ export const modals = () => {
 
 	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
 	bindModal('.phone_link', '.popup', '.popup .popup_close');
-	showModalByTime('.popup', 3000);
+	// showModalByTime('.popup', 60000);
 };
---
+++ src/js/modules/tabs.js
@@ -0,0 +1,34 @@
+export const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+  const header = document.querySelector(headerSelector);
+  const tab = document.querySelectorAll(tabSelector);
+  const content = document.querySelectorAll(contentSelector);
+
+  function hideTabContent() {
+    content.forEach((item) => (item.style.display = 'none'));
+    tab.forEach((item) => item.classList.remove(activeClass));
+  }
+
+  function showTabContent(i = 0) {
+    content[i].style.display = 'block';
+    tab[i].classList.add(activeClass);
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  header.addEventListener('click', (e) => {
+    const target = e.target;
+    if (
+      target &&
+      (target.classList.contains(tabSelector.slice(1)) ||
+        target.parentNode.classList.contains(tabSelector.slice(1)))
+    ) {
+      tab.forEach((item, i) => {
+        if (target === item || target.parentNode === item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+};
